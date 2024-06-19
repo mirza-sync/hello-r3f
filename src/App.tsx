5b5b070802fea9ffa59123654300c3ed99597069
mirza-sync <mirzasyahmi123@gmail.com>
2024-06-19 22:49:20 +0800

    Add sphere

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import './App.css'
+import { Canvas, ThreeElements } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
-import * as THREE from 'three'
-import { useRef, useState } from 'react'
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
-
-function Box(props: ThreeElements['mesh']) {
-  const meshRef = useRef<THREE.Mesh>(null!)
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  useFrame((_, delta) => (meshRef.current.rotation.x += delta))
+function Earth(props: ThreeElements['mesh']) {
   return (
     <mesh
       {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
     >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <icosahedronGeometry args={[1, 12]} />
+      <meshStandardMaterial color={'lightblue'} />
     </mesh>
   )
 }
@@ -28,11 +16,9 @@ function App() {
   return (
     <>
       <Canvas>
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
+        <OrbitControls />
+        <hemisphereLight args={['#ffffff', '#000000', 0.5]} />
+        <Earth position={[0, 0, 0]} />
       </Canvas>
     </>
   )
